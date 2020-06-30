a6a60663003f1588cb2a34496910aad0a4ec93aa
Jeremy Hon <jehon11@gmail.com>
2020-06-30 22:53:00 +0900

    update content

--- components/ProfilePage/Projects/index.tsx
+++ components/ProfilePage/Projects/index.tsx
@@ -2,23 +2,31 @@ import styled from "styled-components";
 import { EmojiSpan } from "../../Common/Styled";
 import { Container, Modal, Button, Row } from "react-bootstrap";
 import { useState } from "react";
+import { MdClose } from "react-icons/md";
 
 const projects = [
+  {
+    name: "Getintouch",
+    url: "https://getintouch.me",
+    imgUrl: "/images/projects/getintouch.jpg",
+    description:
+      "A self promotion tool to help you find your next role. Created with React with hooks, Typescript, Next.js, Firebase, and served on Vercel",
+  },
+  {
+    name: "360Review",
+    url: "https://360review.jeremyhon.com",
+    githubUrl: "https://github.com/jehon11/employee_review",
+    imgUrl: "/images/projects/360review.jpg",
+    description:
+      "A web-app that allows employees to submit feedback towards each other’s performance review. Created with Rails, React, Postgres and hosted on Heroku",
+  },
   {
     name: "WeEatigo",
     url: "https://eatigo-app.herokuapp.com/",
     githubUrl: "https://github.com/jehon11/restaurant-reservations",
     imgUrl: "/images/projects/eatigo.jpg",
     description:
-      "A restaurant reservation app that delivers discounts for reserving at non-peak hours, thereby smoothing out demand and increasing overall business. A clone of a popular website in Asia",
-  },
-  {
-    name: "360Review",
-    url: "https://employee-reviews.jeremyhon.com",
-    githubUrl: "https://github.com/jehon11/employee_review",
-    imgUrl: "/images/projects/360review.jpg",
-    description:
-      "A web-app that allows employees to submit feedback towards each other’s performance review (React, Rails, Postgres, Heroku)",
+      "A clone of a popular restaurant reservation app in Asia which provides discounts for reserving at non-peak hours, thereby smoothing out demand and increasing overall business. Created with Rails, React, Postgres and hosted on Heroku",
   },
   {
     name: "Slacky",
@@ -26,7 +34,7 @@ const projects = [
     githubUrl: "https://github.com/jehon11/slacky-chat-app",
     imgUrl: "/images/projects/slacky.jpg",
     description:
-      "A Slack-like SPA chat app (React/Redux, Rails, Postgres, Heroku, WebSockets)",
+      "A Slack-like single paged real-time chat application built using React/Redux, Rails, Postgres, WebSockets and hosted on Heroku",
   },
 ];
 const Projects: React.FC = () => {
@@ -41,6 +49,10 @@ const Projects: React.FC = () => {
         <Title>
           Projects<EmojiSpan>🤔</EmojiSpan>
         </Title>
+        <Disclaimer>
+          *The live site may take a few seconds to load initially if it is a
+          cold start
+        </Disclaimer>
         <Container style={{ padding: 0 }}>
           <Row>
             {projects.map((project, index) => (
@@ -56,17 +68,29 @@ const Projects: React.FC = () => {
                   size="lg"
                 >
                   <ModalContent>
+                    <ModalClose onClick={handleClose}>
+                      <MdClose size={25} />
+                    </ModalClose>
                     <ProjectModalImageContainer>
                       <ProjectImage src={project.imgUrl} />
                     </ProjectModalImageContainer>
                     <ProjectDescriptionContainer>
-                      <ProjectLink href={project.githubUrl} target="_blank">
-                        Github Repo
-                      </ProjectLink>
-                      <ProjectLink href={project.url} target="_blank">
-                        Live Site
-                      </ProjectLink>
+                      <h5>{project.name}</h5>
                       <p>{project.description}</p>
+                      {project.githubUrl && (
+                        <ProjectLink href={project.githubUrl} target="_blank">
+                          Github Repo
+                        </ProjectLink>
+                      )}
+                      {project.url && (
+                        <ProjectLink
+                          href={project.url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Live Site
+                        </ProjectLink>
+                      )}
                     </ProjectDescriptionContainer>
                   </ModalContent>
                 </Modal>
@@ -80,7 +104,11 @@ const Projects: React.FC = () => {
 };
 
 const ProjectDescriptionContainer = styled.div`
-  padding: 20px 5%;
+  padding: 40px 5%;
+`;
+
+const Disclaimer = styled.p`
+  color: gray;
 `;
 
 const ProjectLink = styled.a`
@@ -90,7 +118,6 @@ const ProjectLink = styled.a`
   padding: 5px 1px;
   transition: all 0.2s ease;
   margin-right: 25px;
-  margin-bottom: 20px;
 
   &:hover {
     background-color: var(--color-text);
@@ -100,8 +127,15 @@ const ProjectLink = styled.a`
 
 const ProjectCard = styled.div``;
 
+const ModalClose = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`;
+
 const ModalContent = styled.div`
-  height: 75vh;
+  position: relative;
   background-color: var(--color-bg);
   overflow-y: scroll;
 `;
@@ -137,12 +171,13 @@ const ProjectOverlay = styled.div`
 
 const ProjectImage = styled.img`
   width: 70%;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
 `;
 
 const ProjectModalImageContainer = styled.div`
   height: 65%;
   width: 100%;
+  padding: 60px 0;
   background-color: var(--color-invert-transp);
   display: flex;
   justify-content: center;
