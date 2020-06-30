a5d2ca32650c26b78f9163998ae64df84e1e4ed2
Jeremy Hon <jehon11@gmail.com>
2020-06-30 22:53:29 +0900

    update meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { AppProps } from "next/app";
 import "../styles/index.scss";
 import ThemeProvider from "../providers/ThemeProvider";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Jeremy Hon - Software Engineer</title>
+        <meta
+          name="description"
+          content="I'm a full-stack software engineer from Hong Kong currently based in Tokyo, Japan"
+        />
+      </Head>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
